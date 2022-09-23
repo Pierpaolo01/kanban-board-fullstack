b3f0ce031e770d2e759992ffaafb43816f363922
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-09-23 15:52:20 +0200

    Task: mobile header

---
+++ kanban-frontend/src/components/TheLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import IconLogo from "../assets/icons/IconLogo.vue";
+import KanbanButton from "../components/KanbanButton.vue";
+import IconChevronDown from "../assets/icons/IconChevronDown.vue";
+</script>
+
+<template>
+  <div>
+    <header
+      class="px-4 py-6 w-full h-16 bg-white dark:bg-dark-gray flex items-center justify-between"
+    >
+      <div class="flex items-center space-x-4">
+        <IconLogo />
+        <h1 class="text-lg text-black dark:text-white flex items-center">
+          TODO Board name <IconChevronDown class="ml-2" />
+        </h1>
+      </div>
+      <div>
+        <KanbanButton text="+" />
+      </div>
+    </header>
+  </div>
+</template>
---
+++ kanban-frontend/src/stories/TheLayout.stories.ts
@@ -0,0 +1,17 @@
+import TheLayout from "../components/TheLayout.vue";
+
+export default {
+  Title: "TheLayout",
+  component: TheLayout,
+};
+
+const Template = (args: any) => ({
+  components: { TheLayout },
+  setup() {
+    return { args };
+  },
+  template: `<TheLayout />`,
+});
+
+export const MainLayout = Template.bind({});
+TheLayout.args = {};
