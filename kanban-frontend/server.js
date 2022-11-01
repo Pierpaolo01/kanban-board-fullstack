978760f6acbc48420fb133d2e5493402682fc5b4
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-11-01 14:12:20 +0100

    Heroku SPA support

--- kanban-frontend/server.js
+++ kanban-frontend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
-app.get("/.*/", (_, res) => {
+app.get("/*", (_, res) => {
   res.sendFile(__dirname + "/dist/index.html");
 });
 
