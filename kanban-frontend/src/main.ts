ede991a6d45f7023e163806c43b55850508e4540
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-11-03 13:04:06 +0100

    Fix: prod env

--- kanban-frontend/src/main.ts
+++ kanban-frontend/src/main.ts
@@ -11,10 +11,7 @@ import "./assets/main.css";
 
 const app = createApp(App);
 
-axios.defaults.baseURL =
-  import.meta.env.NODE_ENV === "production"
-    ? "https://kanban-node-api.herokuapp.com"
-    : "http://localhost:3000";
+axios.defaults.baseURL = "https://kanban-node-api.herokuapp.com";
 
 axios.interceptors.request.use((request) => {
   const token = localStorage.getItem("token");
